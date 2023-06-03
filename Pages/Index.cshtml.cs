fa1f790f5cf08b8df37fdb70b1dd6dbe3c151c27
jamarl24 <jamarlsarry24@gmail.com>
2023-06-03 22:52:12 +1000

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Grocer.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public int Rating { get; set; }
+        [BindProperty]
+        public string Feedback { get; set; }
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
